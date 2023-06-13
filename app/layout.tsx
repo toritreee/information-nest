21b9f9dcad10f5915af6dfb5214250a5f042e2d0
toritreee <65443082+toritreee@users.noreply.github.com>
2023-06-13 11:58:27 +0900

    PWAに対応してインストールできるようにした

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
+      <link rel="manifest" href="/manifest.json" />
     </html>
   )
 }
